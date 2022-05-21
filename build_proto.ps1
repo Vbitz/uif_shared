118588872f766fcdac1c85b27313dd632a41f32a
Joshua Scarsbrook <jscarsbrook@gmail.com>
2022-05-21 17:01:54 +1000

    Moved each language into a seperate dir

--- build_proto.ps1
+++ build_proto.ps1
@@ -4,11 +4,11 @@ $GoBin = "C:\Users\jscar\go\bin"
 $Protoc = $VcpkgPath + "\installed\x64-windows\tools\protobuf\protoc.exe"
 $GrpcCppPlugin = $VcpkgPath + "\packages\grpc_x64-windows\tools\grpc\grpc_cpp_plugin.exe"
 
-. $Protoc -I. --cpp_out=. .\uif.proto
-. $Protoc -I. --grpc_out=. --plugin=protoc-gen-grpc=$GrpcCppPlugin .\uif.proto
+. $Protoc -I. --cpp_out=cpp .\uif.proto
+. $Protoc -I. --grpc_out=cpp --plugin=protoc-gen-grpc=$GrpcCppPlugin .\uif.proto
 
-python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. uif.proto
+python -m grpc_tools.protoc -I. --python_out=py --grpc_python_out=py uif.proto
 
 $Env:PATH += ";" + $GoBin
 
-. $Protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative uif.proto
+. $Protoc --go_out=go --go_opt=paths=source_relative --go-grpc_out=go --go-grpc_opt=paths=source_relative uif.proto
