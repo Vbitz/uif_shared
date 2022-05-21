6a6dae5f5594a37d3b295031e1e1cf0ffda638cf
Joshua Scarsbrook <jscarsbrook@gmail.com>
2022-05-21 16:44:12 +1000

    Added inital protobuf test

--- build_proto.ps1
+++ build_proto.ps1
@@ -1,4 +1,5 @@
 $VcpkgPath = "C:\Users\jscar\dev\apps\vcpkg"
+$GoBin = "C:\Users\jscar\go\bin"
 
 $Protoc = $VcpkgPath + "\installed\x64-windows\tools\protobuf\protoc.exe"
 $GrpcCppPlugin = $VcpkgPath + "\packages\grpc_x64-windows\tools\grpc\grpc_cpp_plugin.exe"
@@ -7,3 +8,7 @@ $GrpcCppPlugin = $VcpkgPath + "\packages\grpc_x64-windows\tools\grpc\grpc_cpp_pl
 . $Protoc -I. --grpc_out=. --plugin=protoc-gen-grpc=$GrpcCppPlugin .\uif.proto
 
 python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. uif.proto
+
+$Env:PATH += ";" + $GoBin
+
+. $Protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative uif.proto
