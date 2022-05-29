6ed97f1258e19787c7764d955eec246e14b0731c
Joshua Scarsbrook <jscarsbrook@gmail.com>
2022-05-29 16:48:38 +1000

    Added __init__.py for python support

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__), "py"))
 
-import shared.uif_pb2 as uif  # noqa: E402
-import shared.uif_pb2_grpc as uif_grpc  # noqa: E402
+import uif_pb2 as uif  # noqa: E402
+import uif_pb2_grpc as uif_grpc  # noqa: E402
